972fce20183a27b214e06a8bfc7be0d8fee14a5c
Lars Hellemo <lars.hellemo@sintef.no>
2024-06-27 16:00:51 +0200

    Initial structure for documentation

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+using Documenter, DocumenterCitations, EnergyModelsHeat
+
+pages = [
+    "Introduction" => "index.md",
+    "How to" => [
+        "Use surplus heat for DH" => "howto/simple_conversion.md",
+    ],
+    "Background" => "background/background.md",
+    "API reference" => "reference/api.md",
+]
+
+bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"))
+# TODO: Enable modules
+# TODO: Enable doctests
+Documenter.makedocs(
+    sitename = "EnergyModelsHeat",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        edit_link = "main",
+        assets = String[],
+    ),
+    doctest = false,
+    # modules = [EnergyModelsHeat],
+    pages = pages,
+    remotes = nothing,
+    plugins=[bib],
+)
+
+# Documenter.deploydocs(; repo = "github.com/sintefore/PiecewiseAffineApprox.jl.git")
