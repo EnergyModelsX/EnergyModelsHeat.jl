b10782ca6bbb3cd5cf95817838fafc2274e26caa
gonzalodelalamo <gonzalodelalamo@gmail.com>
2025-02-22 10:34:35 +0100

    Update bioCHP.cpp

--- submodels/bioCHP_plant/bioCHP.cpp
+++ submodels/bioCHP_plant/bioCHP.cpp
@@ -5,14 +5,21 @@
 #include "Cost.h"
 #include "Processes.h"
 
-void bioCHP_plant(vector<string> fuel_def, vector<double> Yj, double W_el, double Qk, double Tk_in, double Tk_out){
+void bioCHP_plant(vector<string> fuel_def, vector<double> Yj, double W_el, double Qk, double Tk_in, double Tk_out, vector<double> &Mj, double &C_inv,double &C_op){
 
+	// INPUTS
 	// feed_def: name of each biomass feedstock
 	// Yj: mass fraction of each biomass feedstock
 	// W_el: electric power output (MW_el)
 	// Qk: heat demand (MW)
 	// Tk_in: Return temperature for each heat demand (district heating)
 	// Tk_in: Supply temperature for each heat demand (district heating)
+
+	// OUTPUTS
+	// Mj: Required mass flow of each biomass feedstock
+	// C_inv: Investment cost (M$)
+	// C_op: Annual operating cost (M$)
+
 
 	object bioCHP("plant", "bioCHP_PLANT", "bioCHP_inputs");
 	bioCHP.vct_sp("fuel_def", fuel_def);
@@ -23,7 +30,12 @@ void bioCHP_plant(vector<string> fuel_def, vector<double> Yj, double W_el, doubl
 	bioCHP.fval_p("Tk_out", Tk_out);
 
 	bioCHP_plant_model(bioCHP);
+
+	Mj = bioCHP.vctp("Mj");
+	C_inv = bioCHP.fp("C_inv") * 1e-6;
+	C_op = bioCHP.fp("C_op") * 1e-6;
+
 	export_output_parameters(bioCHP, "bioCHP_outputs");
 	
-
 }
+
