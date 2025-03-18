c06c32f71f8a76ea02d3c1b386488ad8de5d3c3f
gonzalodelalamo <gonzalodelalamo@gmail.com>
2025-03-18 08:18:12 +0100

    Update Thermodynamics_package.h

--- submodels/bioCHP_plant/Thermodynamic_library/Thermodynamics_package.h
+++ submodels/bioCHP_plant/Thermodynamic_library/Thermodynamics_package.h
@@ -3,13 +3,15 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <boost/algorithm/string.hpp>
 #include <string.h>
 #include <vector> 
 #include <math.h>
-#include "thermodynamics.h"
+#include "species_thermodynamics.h"
 #include "Library_Water_Correlations.h"
+/*
+#include <boost/algorithm/string.hpp>
+#include "thermodynamics.h"
 #include "thermodata.h"
 #include "conversion.h"
 #include "thermo_property_models.h"
-
+*/
