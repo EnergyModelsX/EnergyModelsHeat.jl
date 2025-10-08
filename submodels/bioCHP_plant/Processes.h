30a81875845e25858394c08772a46ab5d0f8f23a
Julian Straus <julian.straus@sintef.no>
2025-10-08 09:42:50 +0200

    Removed CHP model

--- submodels/bioCHP_plant/Processes.h
+++
@@ -1,7 +0,0 @@
-#include "Processes_library/Rankine_cycle.h"
-#include "Processes_library/combustion.h"
-#include "Processes_library/flue_gas_cleaning.h"
-#include "Processes_library/bioCHP_plant.h"
-
-
-
