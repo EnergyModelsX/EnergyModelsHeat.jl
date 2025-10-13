d68f6aef78022b2734cafa9e2a59dd6056feed5a
Till Holmes <till.holmes@sintef.no>
2025-10-13 17:41:09 +0200

    Added tests and checks for FixedRateTES

--- test/test_checks.jl
+++ test/test_checks.jl
@@ -232,6 +232,104 @@ end
     EMB.TEST_ENV = false
 end
 
+# Test that the fields of a FixedRateTES are correctly checked
+# - check_node(n::FixedRateTES, 𝒯, modeltype::EnergyModel)
+@testitem "Checks - FixedRateTES" begin
+    using EnergyModelsBase
+    using TimeStruct
+
+    const EMB = EnergyModelsBase
+
+    # Set the global to true to suppress the error message
+    EMB.TEST_ENV = true
+
+    # Resources used in the analysis
+    Heat = ResourceCarrier("Heat", 0.0)
+    CO2 = ResourceEmit("CO2", 1.0)
+
+    # Function for setting up the system for testing a `FixedRateTES` node
+    function check_graph(;
+        level_cap = FixedProfile(20),
+        level_opex = FixedProfile(0.8),
+        stor_res = Heat,
+        heat_loss_factor = 0.05,
+        level_charge =0.5,
+        level_discharge = 1.0,
+        input = Dict(Heat => 1),
+        output = Dict(Heat => 1),
+    )
+        products = [Heat, CO2]
+        # Creation of the source and sink module as well as the arrays used for nodes and links
+        TES = EnergyModelsHeat.FixedRateTES{CyclicRepresentative}(
+            "TES",
+            StorCapOpexFixed(level_cap, level_opex),
+            stor_res,
+            heat_loss_factor,
+            level_charge,
+            level_discharge,
+            input,
+            output,
+        )
+        heat_source = RefSource(
+            "Source",
+            OperationalProfile([1, 2, 3, 4, 5, 4, 3, 2, 1, 0]),
+            FixedProfile(0),
+            FixedProfile(0),
+            Dict(Heat => 1),
+        )
+        heat_demand = RefSink(
+            "heat_demand",
+            OperationalProfile([1, 1, 1, 1, 1, 3, 3, 3, 3, 3]),
+            Dict(:surplus => FixedProfile(0), :deficit => FixedProfile(1e6)),
+            Dict(Heat => 1),
+        )
+
+        nodes = [TES, heat_source, heat_demand]
+        links = [
+            Direct("source-TES", nodes[2], nodes[1]),
+            Direct("source-demand", nodes[2], nodes[3]),
+            Direct("TES-demand", nodes[1], nodes[3]),
+        ]
+
+        # Creation of the time structure and the used global data
+        op_per_strat = 8760.0
+        T = TwoLevel(2, 2, SimpleTimes(10, 1); op_per_strat)
+        modeltype = OperationalModel(
+            Dict(CO2 => FixedProfile(10)),
+            Dict(CO2 => FixedProfile(0)),
+            CO2,
+        )
+
+        # Input data structure
+        case = Case(T, products, [nodes, links], [[get_nodes, get_links]])
+        return create_model(case, modeltype), case, modeltype
+    end
+
+    # Test that a wrong capacity is caught by the checks
+    @test_throws AssertionError check_graph(; level_cap = FixedProfile(-25))
+
+    # Test that a wrong heat loss factor is caught by the checks
+    @test_throws AssertionError check_graph(; heat_loss_factor = 1.2)
+    @test_throws AssertionError check_graph(; heat_loss_factor = -0.4)
+
+    # Test that a wrong input or output is caught by the checks
+    @test_throws AssertionError check_graph(; input = Dict(Heat => -0.5))
+    @test_throws AssertionError check_graph(; output = Dict(Heat => -0.5))
+
+    # Test that a wrong fixed OPEX is caught by the checks
+    @test_throws AssertionError check_graph(; level_opex = FixedProfile(-5))
+    @test_throws AssertionError check_graph(; level_opex = OperationalProfile([10]))
+
+    # Test that a wrong charge or discharge factor are caught by the checks
+    @test_throws AssertionError check_graph(; level_charge = -0.4)
+    @test_throws AssertionError check_graph(; level_charge = 1.2)
+    @test_throws AssertionError check_graph(; level_discharge = -0.4)
+    @test_throws AssertionError check_graph(; level_discharge = 1.2)
+
+    # Set the global again to false
+    EMB.TEST_ENV = false
+end
+
 # Test that the fields of a DHPipe are correctly checked
 # - EMB.check_link(l::DHPipe, 𝒯,  modeltype::EnergyModel, check_timeprofiles::Bool)
 @testitem "Checks - DHPipe" begin
--- test/test_heat_storage.jl
+++ test/test_heat_storage.jl
@@ -96,3 +96,113 @@
     @test real_heatlosses ≈ 1.3333 atol = 0.01
     @test heat_stored ≈ 3.3333 atol = 0.01
 end
+
+@testitem "FixedRateTES" begin
+    using EnergyModelsBase
+    using HiGHS
+    using JuMP
+    using TimeStruct
+
+    const EMH = EnergyModelsHeat
+
+    function generate_data()
+
+        # Define the different resources and their emission intensity in tCO2/MWh
+        power    = ResourceCarrier("Power", 0.0)
+        heat_sur = ResourceCarrier("Heat_surplus", 0.0)
+        heat_use = ResourceCarrier("Heat_usable", 0.0)
+        CO₂      = ResourceEmit("CO₂", 1.0)
+        products = [power, heat_sur, heat_use, CO₂]
+
+        op_duration = 2 # Each operational period has a duration of 2
+        op_number = 4   # There are in total 4 operational periods
+        operational_periods = SimpleTimes(op_number, op_duration)
+
+        op_per_strat = op_duration * op_number
+
+        # Creation of the time structure and global data
+        T = TwoLevel(2, 1, operational_periods; op_per_strat)
+        model = OperationalModel(
+            Dict(CO₂ => FixedProfile(10)),  # Emission cap for CO₂ in t/8h
+            Dict(CO₂ => FixedProfile(0)),   # Emission price for CO₂ in EUR/t
+            CO₂,                            # CO₂ instance
+        )
+
+        nodes = [
+            RefSource(
+                "surplus heat source",      # Node id
+                FixedProfile(0.85),         # Capacity in MW
+                FixedProfile(0),            # Variable OPEX in EUR/MW
+                FixedProfile(0),            # Fixed OPEX in EUR/8h
+                Dict(heat_sur => 1),        # Output from the Node, in this gase, heat_sur
+            ),
+            RefNetworkNode(
+                "HeatPump",
+                FixedProfile(0.5),
+                FixedProfile(0),
+                FixedProfile(0),
+                Dict(heat_sur => 1),
+                Dict(heat_use => 1),
+            ),
+            EMH.FixedRateTES{CyclicStrategic}(
+                "TES",
+                StorCap(FixedProfile(1)),
+                heat_use,
+                0.2,
+                0.25,
+                0.1,
+                Dict(heat_use => 1),
+                Dict(heat_use => 1),
+            ),
+            RefSink(
+                "heat demand",              # Node id
+                OperationalProfile([0.1, 0.1, 0.2, 0.8]), # Demand in MW
+                Dict(:surplus => FixedProfile(0), :deficit => FixedProfile(1e6)),
+                # Line above: Surplus and deficit penalty for the node in EUR/MWh
+                Dict(heat_use => 1),           # Energy demand and corresponding ratio
+            ),
+        ]
+
+        # Connect all nodes with the availability node for the overall energy/mass balance
+        links = [
+            Direct("source-HP", nodes[1], nodes[2], Linear()),
+            Direct("HP-demand", nodes[2], nodes[4], Linear()),
+            Direct("HP-TES", nodes[2], nodes[3], Linear()),
+            Direct("TES-demand", nodes[3], nodes[4], Linear()),
+        ]
+
+        # Input data structure
+        case = Case(T, products, [nodes, links], [[get_nodes, get_links]])
+        return (; case, model, nodes, products, T, op_duration)
+    end
+    case, model, nodes, products, T, op_duration = generate_data()
+    optimizer = optimizer_with_attributes(HiGHS.Optimizer, MOI.Silent() => true)
+    m = run_model(case, model, optimizer)
+
+    heat_use = products[3]
+
+    # Test that the expected heat loss ratio is calculated
+    heatloss = EMH.heat_loss_factor(nodes[3])
+
+    heat_input = sum(JuMP.value(m[:flow_in][nodes[3], t, heat_use]) for t ∈ T) * op_duration
+    heat_output =
+        sum(JuMP.value(m[:flow_out][nodes[3], t, heat_use]) for t ∈ T) * op_duration
+    heat_stored = sum(JuMP.value(m[:stor_level][nodes[3], t]) for t ∈ T)
+
+    calculated_max_charge_rate = maximum([JuMP.value(m[:stor_charge_use][nodes[3], t]) for t in collect(T)])
+    calculated_max_discharge_rate = maximum([JuMP.value(m[:stor_discharge_use][nodes[3], t]) for t in collect(T)])
+
+    real_max_charge_rate = nodes[3].level_charge
+    real_max_discharge_rate = nodes[3].level_discharge
+
+    # Check that the heat delivered matches the expected ratio of heat stored
+    calculated_heatlosses = heat_stored * heatloss * op_duration
+    real_heatlosses = heat_input - heat_output
+    @test real_heatlosses ≈ calculated_heatlosses atol = 0.01
+    @test real_heatlosses ≈ 0.2666 atol = 0.01
+    @test heat_stored ≈ 0.6666 atol = 0.01
+    @test calculated_max_charge_rate ≤ real_max_charge_rate
+    @test calculated_max_discharge_rate ≤ real_max_discharge_rate
+
+
+end
