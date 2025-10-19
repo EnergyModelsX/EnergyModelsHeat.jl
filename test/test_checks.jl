90efc41224b2de00661fe6749fa447305dd3c827
Julian Straus <julian.straus@sintef.no>
2025-10-19 14:05:24 +0200

    Unify the HeatPump tests

--- test/test_checks.jl
+++ test/test_checks.jl
@@ -31,88 +31,18 @@ end
 
 # Test that the fields of a HeatPump are correctly checked
 # - check_node(n::HeatPump, 𝒯, modeltype::EnergyModel)
-@testitem "Checks - HeatPump" begin
+@testitem "Checks - HeatPump" setup = [HeatPumpTestData] begin
     using EnergyModelsBase
     using TimeStruct
-    const EMB = EnergyModelsBase
 
     # Set the global to true to suppress the error message
-    EMB.TEST_ENV = true
+    EnergyModelsBase.TEST_ENV = true
 
     # Resources used in the analysis
-    Power = ResourceCarrier("Power", 0.0)
-    Heat = ResourceCarrier("Heat", 0.0)
-    CO2 = ResourceEmit("CO2", 1.0)
+    heat = ResourceCarrier("Heat", 0.0)
 
     # Function for setting up the system for testing a `HeatPump` node
-    function check_graph(;
-        cap = FixedProfile(20),
-        cap_lower_bound = 0.2,
-        t_source = FixedProfile(20),
-        t_sink = FixedProfile(80),
-        eff_carnot = FixedProfile(0.5),
-        input_heat = Heat,
-        driving_force = Power,
-        opex_var = FixedProfile(0),
-        opex_fixed = FixedProfile(0),
-        output = Dict(Heat => 1),
-    )
-        products = [Power, Heat, CO2]
-        # Creation of the source and sink module as well as the arrays used for nodes and links
-        heat_pump = EnergyModelsHeat.HeatPump(
-            "heat_pump",
-            cap,
-            cap_lower_bound,
-            t_source,
-            t_sink,
-            eff_carnot,
-            input_heat,
-            driving_force,
-            opex_var,
-            opex_fixed,
-            output,
-        )
-        heat_source = RefSource(
-            "heat_source",
-            FixedProfile(20),
-            FixedProfile(0.1),
-            FixedProfile(0),
-            Dict(Heat => 1),
-        )
-        power_source = RefSource(
-            "power_source",
-            FixedProfile(20),
-            FixedProfile(0.3),
-            FixedProfile(0),
-            Dict(Power => 1),
-        )
-        heat_demand = RefSink(
-            "heat_demand",
-            FixedProfile(10),
-            Dict(:surplus => FixedProfile(0), :deficit => StrategicProfile([1e3, 2e2])),
-            Dict(Heat => 1),
-        )
-
-        nodes = [heat_pump, heat_source, power_source, heat_demand]
-        links = [
-            Direct("hp-sink", nodes[1], nodes[4]),
-            Direct("heat_source-hp", nodes[2], nodes[1]),
-            Direct("power_source-hp", nodes[3], nodes[1]),
-        ]
-
-        # Creation of the time structure and the used global data
-        op_per_strat = 8760.0
-        T = TwoLevel(2, 2, SimpleTimes(10, 1); op_per_strat)
-        modeltype = OperationalModel(
-            Dict(CO2 => FixedProfile(10)),
-            Dict(CO2 => FixedProfile(0)),
-            CO2,
-        )
-
-        # Input data structure
-        case = Case(T, products, [nodes, links], [[get_nodes, get_links]])
-        return create_model(case, modeltype), case, modeltype
-    end
+    check_graph = HeatPumpTestData.hp_test_case
 
     # Test that a wrong capacity is caught by the checks
     @test_throws AssertionError check_graph(; cap = FixedProfile(-25))
@@ -126,16 +56,16 @@ end
     @test_throws AssertionError check_graph(; eff_carnot = FixedProfile(1.2))
 
     # Test that a wrong source and sink temperature is caught by the checks
-    @test_throws AssertionError check_graph(; t_source = FixedProfile(90))
+    @test_throws AssertionError check_graph(; t_source = FixedProfile(100))
 
     # Test that a wrong fixed OPEX is caught by the checks
     @test_throws AssertionError check_graph(; opex_fixed = FixedProfile(-5))
 
     # Test that a wrong output dictionary is caught
-    @test_throws AssertionError check_graph(; output = Dict(Heat => -0.9))
+    @test_throws AssertionError check_graph(; output = Dict(heat => -0.9))
 
     # Set the global again to false
-    EMB.TEST_ENV = false
+    EnergyModelsBase.TEST_ENV = false
 end
 
 # Test that the fields of a ThermalEnergyStorage are correctly checked
