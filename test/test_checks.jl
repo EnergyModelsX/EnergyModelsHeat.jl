af9df727256e9b5067b22cf6b528b6c5d5a8f9aa
Julian Straus <julian.straus@sintef.no>
2025-10-20 12:57:36 +0200

    Unify the TES tests

--- test/test_checks.jl
+++ test/test_checks.jl
@@ -69,96 +69,40 @@ end
 end
 
 # Test that the fields of a ThermalEnergyStorage are correctly checked
-# - check_node(n::ThermalEnergyStorage, 𝒯, modeltype::EnergyModel)
-@testitem "Checks - ThermalEnergyStorage" begin
+# - EMB.check_node(n::AbstractTES{T}, 𝒯, modeltype::EnergyModel, check_timeprofiles::Bool)
+@testitem "Checks - ThermalEnergyStorage" setup = [TESTestData] begin
     using EnergyModelsBase
     using TimeStruct
 
-    const EMB = EnergyModelsBase
-
     # Set the global to true to suppress the error message
-    EMB.TEST_ENV = true
-
-    # Resources used in the analysis
-    Heat = ResourceCarrier("Heat", 0.0)
-    CO2 = ResourceEmit("CO2", 1.0)
+    EnergyModelsBase.TEST_ENV = true
 
     # Function for setting up the system for testing a `ThermalEnergyStorage` node
-    function check_graph(;
-        charge_cap = FixedProfile(5),
-        level_cap = FixedProfile(20),
-        charge_opex = FixedProfile(0.5),
-        level_opex = FixedProfile(0.8),
-        stor_res = Heat,
-        heat_loss_factor = 0.05,
-        input = Dict(Heat => 1),
-        output = Dict(Heat => 1),
-        oper = SimpleTimes(10, 1),
-        stor_behav = CyclicRepresentative,
-    )
-        products = [Heat, CO2]
-        # Creation of the source and sink module as well as the arrays used for nodes and links
-        TES = EnergyModelsHeat.ThermalEnergyStorage{stor_behav}(
-            "TES",
-            StorCapOpexFixed(charge_cap, charge_opex),
-            StorCapOpexFixed(level_cap, level_opex),
-            stor_res,
-            heat_loss_factor,
-            input,
-            output,
-        )
-        heat_source = RefSource(
-            "Source",
-            OperationalProfile([1, 2, 3, 4, 5, 4, 3, 2, 1, 0]),
-            FixedProfile(0),
-            FixedProfile(0),
-            Dict(Heat => 1),
-        )
-        heat_demand = RefSink(
-            "heat_demand",
-            OperationalProfile([1, 1, 1, 1, 1, 3, 3, 3, 3, 3]),
-            Dict(:surplus => FixedProfile(0), :deficit => FixedProfile(1e6)),
-            Dict(Heat => 1),
-        )
+    check_graph = TESTestData.tes_test_case
 
-        nodes = [TES, heat_source, heat_demand]
-        links = [
-            Direct("source-TES", nodes[2], nodes[1]),
-            Direct("source-demand", nodes[2], nodes[3]),
-            Direct("TES-demand", nodes[1], nodes[3]),
-        ]
-
-        # Creation of the time structure and the used global data
-        op_per_strat = 8760.0
-        T = TwoLevel(2, 2, oper; op_per_strat)
-        modeltype = OperationalModel(
-            Dict(CO2 => FixedProfile(10)),
-            Dict(CO2 => FixedProfile(0)),
-            CO2,
-        )
-
-        # Input data structure
-        case = Case(T, products, [nodes, links], [[get_nodes, get_links]])
-        return create_model(case, modeltype), case, modeltype
-    end
+    # Resources used in the analysis
+    heat = ResourceCarrier("Heat", 0.0)
 
     # Test that a wrong capacity is caught by the checks
     @test_throws AssertionError check_graph(; charge_cap = FixedProfile(-25))
     @test_throws AssertionError check_graph(; level_cap = FixedProfile(-25))
+    @test_throws AssertionError check_graph(; discharge_cap = FixedProfile(-25))
 
     # Test that a wrong heat loss factor is caught by the checks
     @test_throws AssertionError check_graph(; heat_loss_factor = 1.2)
     @test_throws AssertionError check_graph(; heat_loss_factor = -0.4)
 
     # Test that a wrong input or output is caught by the checks
-    @test_throws AssertionError check_graph(; input = Dict(Heat => -0.5))
-    @test_throws AssertionError check_graph(; output = Dict(Heat => -0.5))
+    @test_throws AssertionError check_graph(; input = Dict(heat => -0.5))
+    @test_throws AssertionError check_graph(; output = Dict(heat => -0.5))
 
     # Test that a wrong fixed OPEX is caught by the checks
     @test_throws AssertionError check_graph(; charge_opex = FixedProfile(-5))
     @test_throws AssertionError check_graph(; charge_opex = OperationalProfile([10]))
     @test_throws AssertionError check_graph(; level_opex = FixedProfile(-5))
     @test_throws AssertionError check_graph(; level_opex = OperationalProfile([10]))
+    @test_throws AssertionError check_graph(; discharge_opex = FixedProfile(-5))
+    @test_throws AssertionError check_graph(; discharge_opex = OperationalProfile([10]))
 
     # Test that the warning regarding the time structure is thrown
     msg =
@@ -166,117 +110,60 @@ end
         "`RepresentativePeriods` " *
         "results in errors for the calculation of the heat loss. It is not advised " *
         "to utilize this `StorageBehavior`. Use instead `CyclicRepresentative`."
-    oper = RepresentativePeriods(8760, [0.5, 0.5], SimpleTimes(10, 1))
-    @test_logs (:warn, msg) check_graph(; stor_behav = CyclicStrategic, oper)
+    type = ThermalEnergyStorage{CyclicStrategic}
+    oper = RepresentativePeriods(8760, [0.5, 0.5], SimpleTimes(4, 1))
+    @test_logs (:warn, msg) check_graph(; type, oper)
 
     # Set the global again to false
-    EMB.TEST_ENV = false
+    EnergyModelsBase.TEST_ENV = false
 end
 
 # Test that the fields of a BoundRateTES are correctly checked
-# - check_node(n::BoundRateTES, 𝒯, modeltype::EnergyModel)
-@testitem "Checks - BoundRateTES" begin
+# - EMB.check_node(n::BoundRateTES{T}, 𝒯, modeltype::EnergyModel, check_timeprofiles::Bool)
+@testitem "Checks - BoundRateTES" setup = [TESTestData] begin
     using EnergyModelsBase
     using TimeStruct
 
-    const EMB = EnergyModelsBase
-
     # Set the global to true to suppress the error message
-    EMB.TEST_ENV = true
-
-    # Resources used in the analysis
-    Heat = ResourceCarrier("Heat", 0.0)
-    CO2 = ResourceEmit("CO2", 1.0)
+    EnergyModelsBase.TEST_ENV = true
 
     # Function for setting up the system for testing a `BoundRateTES` node
-    function check_graph(;
-        level_cap = FixedProfile(20),
-        level_opex = FixedProfile(0.8),
-        stor_res = Heat,
-        heat_loss_factor = 0.05,
-        level_charge = 0.5,
-        level_discharge = 1.0,
-        input = Dict(Heat => 1),
-        output = Dict(Heat => 1),
-        oper = SimpleTimes(10, 1),
-        stor_behav = CyclicRepresentative,
-    )
-        products = [Heat, CO2]
-        # Creation of the source and sink module as well as the arrays used for nodes and links
-        TES = BoundRateTES{stor_behav}(
-            "TES",
-            StorCapOpexFixed(level_cap, level_opex),
-            stor_res,
-            heat_loss_factor,
-            level_charge,
-            level_discharge,
-            input,
-            output,
-        )
-        heat_source = RefSource(
-            "Source",
-            OperationalProfile([1, 2, 3, 4, 5, 4, 3, 2, 1, 0]),
-            FixedProfile(0),
-            FixedProfile(0),
-            Dict(Heat => 1),
-        )
-        heat_demand = RefSink(
-            "heat_demand",
-            OperationalProfile([1, 1, 1, 1, 1, 3, 3, 3, 3, 3]),
-            Dict(:surplus => FixedProfile(0), :deficit => FixedProfile(1e6)),
-            Dict(Heat => 1),
-        )
+    check_graph = TESTestData.tes_test_case
 
-        nodes = [TES, heat_source, heat_demand]
-        links = [
-            Direct("source-TES", nodes[2], nodes[1]),
-            Direct("source-demand", nodes[2], nodes[3]),
-            Direct("TES-demand", nodes[1], nodes[3]),
-        ]
-
-        # Creation of the time structure and the used global data
-        op_per_strat = 8760.0
-        T = TwoLevel(2, 2, oper; op_per_strat)
-        modeltype = OperationalModel(
-            Dict(CO2 => FixedProfile(10)),
-            Dict(CO2 => FixedProfile(0)),
-            CO2,
-        )
-
-        # Input data structure
-        case = Case(T, products, [nodes, links], [[get_nodes, get_links]])
-        return create_model(case, modeltype), case, modeltype
-    end
+    # Resources used in the analysis
+    heat = ResourceCarrier("Heat", 0.0)
 
     # Test that a wrong capacity is caught by the checks
-    @test_throws AssertionError check_graph(; level_cap = FixedProfile(-25))
+    type = BoundRateTES
+    @test_throws AssertionError check_graph(; type, level_cap = FixedProfile(-25))
 
     # Test that a wrong heat loss factor is caught by the checks
-    @test_throws AssertionError check_graph(; heat_loss_factor = 1.2)
-    @test_throws AssertionError check_graph(; heat_loss_factor = -0.4)
+    @test_throws AssertionError check_graph(; type, heat_loss_factor = 1.2)
+    @test_throws AssertionError check_graph(; type, heat_loss_factor = -0.4)
 
     # Test that a wrong input or output is caught by the checks
-    @test_throws AssertionError check_graph(; input = Dict(Heat => -0.5))
-    @test_throws AssertionError check_graph(; output = Dict(Heat => -0.5))
+    @test_throws AssertionError check_graph(; type, input = Dict(heat => -0.5))
+    @test_throws AssertionError check_graph(; type, output = Dict(heat => -0.5))
 
     # Test that a wrong fixed OPEX is caught by the checks
-    @test_throws AssertionError check_graph(; level_opex = FixedProfile(-5))
-    @test_throws AssertionError check_graph(; level_opex = OperationalProfile([10]))
+    @test_throws AssertionError check_graph(; type, level_opex = FixedProfile(-5))
+    @test_throws AssertionError check_graph(; type, level_opex = OperationalProfile([10]))
 
     # Test that a wrong charge or discharge factor are caught by the checks
-    @test_throws AssertionError check_graph(; level_charge = -0.4)
-    @test_throws AssertionError check_graph(; level_discharge = -0.4)
+    @test_throws AssertionError check_graph(; type, level_charge = -0.4)
+    @test_throws AssertionError check_graph(; type, level_discharge = -0.4)
 
     # Test that the warning regarding the time structure is thrown
     msg =
         "Using `CyclicStrategic` with a `BoundRateTES` and `RepresentativePeriods` " *
         "results in errors for the calculation of the heat loss. It is not advised " *
         "to utilize this `StorageBehavior`. Use instead `CyclicRepresentative`."
-    oper = RepresentativePeriods(8760, [0.5, 0.5], SimpleTimes(10, 1))
-    @test_logs (:warn, msg) check_graph(; stor_behav = CyclicStrategic, oper)
+    type = BoundRateTES{CyclicStrategic}
+    oper = RepresentativePeriods(8760, [0.5, 0.5], SimpleTimes(4, 1))
+    @test_logs (:warn, msg) check_graph(; type, oper)
 
     # Set the global again to false
-    EMB.TEST_ENV = false
+    EnergyModelsBase.TEST_ENV = false
 end
 
 # Test that the fields of a DHPipe are correctly checked
