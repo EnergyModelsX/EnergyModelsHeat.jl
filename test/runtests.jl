3e40d8cd6b8d4f216059a2bc070ea1cd8233e40e
Jon Vegard Venås <jonvegard.venas@sintef.no>
2025-12-16 21:14:10 +0100

    Skip EMGUI related tests for now.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,4 @@ using TestItemRunner
 using EnergyModelsHeat
 using PrettyTables
 
-# Create a flag to detect if we are running in CI
-is_ci::Bool = get(ENV, "CI", "false") == "true"
-
-# Run all package tests, but skip those requiring EnergyModelsGUI in CI.
-# Make sure to run these locally before push to GitHub.
-# Locally, these tests can be run with:
-# julia --project=. -e 'import Pkg; Pkg.add("EnergyModelsGUI"); Pkg.instantiate(); Pkg.test()'
-@run_package_tests filter = ti -> !(is_ci && (:requires_emgui ∈ ti.tags))
+@run_package_tests
